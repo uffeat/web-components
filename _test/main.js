2010c5e3c8837e0b48d4764df7af75fce1619909
Uffe <uat@aperitas.com>
2022-04-14 23:45:06 +0700

    h

---
+++ _test/main.js
@@ -0,0 +1,15 @@
+//This works:
+//import * as components from 'https://cdn.jsdelivr.net/gh/uffeat/web-components@v0.0.1/components.js';
+
+//This works:
+//import * as components from 'https://uffeat.github.io/web-components/components.js';
+
+//This works:
+import * as components from 'https://uffeat.github.io/web-components-pkg/components.js';
+
+//... but does not work from Parcel!!!
+
+
+//TODO: Manage release versions.
+
+//TODO: Check that components.js actually works (all options above).
